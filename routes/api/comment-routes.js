11d01aadc1a8c39c2beee98b9a04f366ddd5ffaf
Lawrence Rivales <lrivales@gmail.com>
2021-11-10 22:29:03 -0400

    connect to jawsdb

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -25,7 +25,22 @@ router.post('/', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+  Comment.destroy(
+    {
+      where: {id: req.params.id}
+    }
+  )
+  .then(dbCommentData => {
+    if (!dbCommentData) {
+      res.status(400).json({ message: 'No comment found with this ID.'});
+      return;
+    }
+    res.json(dbCommentData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
