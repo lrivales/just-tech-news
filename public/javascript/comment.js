e4b27890cf306bc0d5ae5e8f0b79d57c5c639ff7
Lawrence Rivales <lrivales@gmail.com>
2021-11-18 20:36:04 -0500

    na

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,11 +2,12 @@ async function commentFormHandler(event) {
     event.preventDefault();
   
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-  
-    // ask anthony about how this works
-    const post_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+
+    const url = window.location.toString().split('/');
+    
+    console.log(url);
+
+    const post_id = url[url.length - 1];
 
     if (comment_text) {
         const response = await fetch('/api/comments', {
